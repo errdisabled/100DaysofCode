2d55923bb73309e25ec171cb779b4f8f83d1ddc8
errdisabled <alex.perkins@protonmail.com>
2021-04-11 22:24:44 -0400

    first day 2 exercise

---
+++ Day2/day-2-1-exercise.py
@@ -0,0 +1,11 @@
+# number adder
+
+two_digit_number = input("Type a two digit number: ")
+
+first_digit = two_digit_number[0]
+
+second_digit = two_digit_number[1]
+
+combined = int(first_digit) + int(second_digit)
+
+print(combined)
