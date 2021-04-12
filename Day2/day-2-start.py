1eff8b41e516ebcc63d2670dddc2d4187bf49e3a
errdisabled <alex.perkins@protonmail.com>
2021-04-11 22:13:52 -0400

    updated

---
+++ Day2/day-2-start.py
@@ -0,0 +1,11 @@
+# Data Types
+
+# String
+
+# print("Hello"[4])
+
+num_char = len(input("What is your name?"))
+
+new_num_char = str(num_char)
+
+print("Your name has " + new_num_char + " characters.")
