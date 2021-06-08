beef21411a5831598ead371d5917c549d804f4b9
errdisabled <alex.perkins@protonmail.com>
2021-06-07 21:29:59 -0400

    not sure what was changed here

---
+++ Day4/day-4-3-exercise.py
@@ -0,0 +1,13 @@
+# Don't change the code below
+row1 = [" ", " ", " "]
+row2 = [" ", " ", " "]
+row3 = [" ", " ", " "]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# Don't change the code above
+
+column = int(position[0])
+row = int(position[1])
+
+print(map[column - 1])
